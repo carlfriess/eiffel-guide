555eb04bdb0090a98b692833815cbfcc818ca4d0
Carl Friess <carl.friess@me.com>
2015-11-18 18:18:19 +0100

    Added progression indication

--- sidebar.js
+++ sidebar.js
@@ -1,8 +1,23 @@
 angular.module('sidebar', [])
-.controller('SidebarController', ["$window", function($window){
+.controller('SidebarController', ["$window", "$scope", function($window, $scope){
     this.scrollTo = function (index) {
         $window.scrollTo(0, document.getElementsByTagName("h2").item(index).offsetTop - 25);
     };
+    angular.element($window).on('scroll', function(){
+
+        var i = 0,
+            h2 = document.getElementsByTagName("h2");
+
+        while (i < h2.length) {
+            if (h2.item(i).offsetTop >= $window.scrollY + 75)
+                break;
+            i++;
+        }
+
+        $scope.sidebarCtrl.highlighted = i - 1;
+        $scope.$apply();
+
+    });
 }])
 .directive('sidebar', function() {
     return {
@@ -13,7 +28,7 @@ angular.module('sidebar', [])
                 h2 = document.getElementsByTagName("h2");
 
             for (var i = 0; i < h2.length; i++) {
-                html += "<a href=\"\" ng-click=\"sidebarCtrl.scrollTo(" + i + ")\">" + h2.item(i).innerText + "</a>";
+                html += "<a href=\"\" ng-click=\"sidebarCtrl.scrollTo(" + i + ")\" ng-class=\"{'highlighted':( " + i + " <= sidebarCtrl.highlighted)}\">" + h2.item(i).innerText + "</a>";
             }
 
             html += "</div></div>";
